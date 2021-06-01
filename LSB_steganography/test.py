4585436eed9992a72532cf2443f7dad2a098bf08
RonsonGallery <57090631+RonsonGallery@users.noreply.github.com>
2021-06-01 20:43:27 +0300

    Add files via upload

---
+++ LSB_steganography/test.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+import LsbSteg
+
+import canny_edge_detector
+
+
+
+
+
+message = "This is a hidden flower in an image"
+
+imageFilename = "BW-using-curves.jpg"
+img = Image.open(imageFilename)
+detect = canny_edge_detector()
+detect.img = img
+
+newImageFilename = "stego_stars_background"
+
+newImg = LsbSteg.encodeLSB(message, imageFilename, newImageFilename)
+if not newImg is None:
+        print("Stego image created.")
+
+print("Decoding...")
+message = LsbSteg.decodeLSB("stego_stars_background.png")
+print("Final message: ", message)
