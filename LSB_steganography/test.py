66e6e181c15c2b04cf37a6c68c3a3f289e1159b1
Sunspawn <nikolay.babkin@gmail.com>
2021-07-09 13:21:09 +0300

    moved file names to top of main

--- LSB_steganography/test.py
+++ LSB_steganography/test.py
@@ -8,22 +8,21 @@ from CaesarCipher import CaesarCipher
 
 if __name__ == "__main__":
         key = 5
+        imageFilename = "BW-using-curves.jpg"
+        newFileName = "stego_stars_background.png"
 
         message = "This is a hidden flower in an image"
 
         encMessage = CaesarCipher.encrypt(message, key)
 
-        imageFilename = "BW-using-curves.jpg"
         img = Image.open(imageFilename)
         detect = canny_edge_detector.cannyEdgeDetector(img)
 
-        newImageFilename = "stego_stars_background"
-
-        newImg = LsbSteg.encodeLSB(encMessage, imageFilename, newImageFilename)
+        newImg = LsbSteg.encodeLSB(encMessage, imageFilename, newFileName)
         if newImg is not None:
                 print("Stego image created.")
 
         print("Decoding...")
-        rawMessage = LsbSteg.decodeLSB("stego_stars_background.png")
+        rawMessage = LsbSteg.decodeLSB(newFileName)
         message = CaesarCipher.decrypt(rawMessage, key)
         print("Final message: ", message)
